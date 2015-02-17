da6c268ac41558492eaa1fb7e44d01c03d7f3333
John Zhang <john@zhang.io>
2015-02-16 17:10:20 -0800

    got the fetch and add to work

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,100 @@
 // YOUR CODE HERE:
+// var app = {
+//   server: "https://api.parse.com/1/classes/chatterbox",
+//   init: function() {},
+//   send: function(message) {
+//     $.ajax({
+//       // always use this url
+//       url: 'https://api.parse.com/1/classes/chatterbox',
+//       type: 'POST',
+//       data: JSON.stringify(message),
+//       contentType: 'application/json',
+//       success: function (data) {
+//         console.log('chatterbox: Message sent');
+//       },
+//       error: function (data) {
+//         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//         console.error('chatterbox: Failed to send message');
+//       }
+//     });
+//   },
+//   fetch: function(){
+//     $.ajax({
+//       url: app.server,
+//       type: 'GET',
+//       dataType: 'application/json',
+//       success: function(data){
+//         console.log('chatterbox: message received');
+//       },
+//       error: function(data){
+//         console.error('chatterbox: failed to receive message');
+//       }
+//     });
+//   },
+//   clearMessages: function() {
+//     $.ajax({
+//       url: app.server,
+//       type: 'DELETE',
+//       data: message,
+//       contentType: 'application/json',
+//       success: function(data){
+//         console.log('chatterbox: message deleted');
+//       },
+//       error: function(data){
+//         console.error('chatterbox: failed to delete message');
+//       }
+//     });
+//   }
+// };
 
+var App = function() { 
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
+};
+
+App.prototype.init = function() {
+
+};
+
+App.prototype.fetch = function(objectID) {
+  $.ajax({
+    url: this.server+"/"+objectID,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data){
+      console.log('chatterbox: message received');
+      console.log(data);
+      $('#main').append('<div>' +JSON.stringify(data) + '</div>');
+    },
+    error: function(data){
+      console.error('chatterbox: failed to receive message');
+    },
+    // $('#main').append('div')
+  });
+};
+
+App.prototype.send = function(message) {
+  $.ajax({
+    // always use this url
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent. Data: ');
+      console.log(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+App.prototype.clearMessages = function() {
+
+};
+
+var app = new App();
+
+app.send({"score":1337,"playerName":"Jack Peterson",});
+app.fetch('69AdwZU98l');
